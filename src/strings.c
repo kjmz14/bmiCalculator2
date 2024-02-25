709f98665cac5e3d628d92167e77c51f1b86536e
Karol <karol.zimmer.kz@gmail.com>
2024-02-25 11:01:48 +0100

    update copyright, make macro more general

--- src/strings.c
+++ src/strings.c
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 Karol Zimmer
+ * Copyright (c) 2023-2024 Karol Zimmer
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,7 +31,7 @@ STRING strCm, strKg, strFt,
 
 extern HINSTANCE hInst;
 
-#define LOAD_STR(a, b) LoadStringW(hInst, a, b, sizeof(STRING)/sizeof(WCHAR))
+#define LOAD_STR(a, b) LoadStringW(hInst, a, b, ARRAYSIZE(b))
 
 void BC2StringsLoad()
 {
